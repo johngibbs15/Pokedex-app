1bd4c3e83013259b062624d23ab784a212d1a4d5
johngibbs15 <john.gibbs901@gmail.com>
2022-09-19 13:05:46 -0400

    Added loop and conditional

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,29 @@
 //Exercise 1.2
 //Pokemon List 
 let pokemonList = [{
-    name: 'Bulbasaur',
-    height: '0.7',
-    type: ['grass', 'poison']
-},
-
-{
-    name: 'Charmander',
-    height: '0.6',
-    type: ['fire', 'dragon']
-
-},
-
-{
-    name: 'Squirtle',
-    height: '0.5',
-    type: ['water', 'Turtle']
-
+	name: 'Bulbasaur',
+	height: '0.7',
+	type: ['grass', 'poison']
+}, {
+	name: 'Charmander',
+	height: '0.6',
+	type: ['fire', 'dragon']
+}, {
+	name: 'Squirtle',
+	height: '0.5',
+	type: ['water', 'Turtle']
+}];
+//Exercise 1.3
+//Loop that iterates over each item in Pokemon List
+//Pokemon's name is written on website's DOM
+for(let i = 0; i < pokemonList.length; i++) {
+//If pokemon is bigger than 0.6 'is huge' is printed in the DOM
+	if(pokemonList[i].height > 0.6) {
+		document.write(pokemonList[i].name + '  ' + 'height:\(' + pokemonList[i].height + '\)  ' + 'is huge!');
+	} else {
+		document.write(pokemonList[i].name + '  ' + 'height:\(' + pokemonList[i].height + '\)  ');
+	}
 }
-];
+
+
+
